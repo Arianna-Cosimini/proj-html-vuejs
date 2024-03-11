b967971cc14491c48773406fc15ec020dd44eab7
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-11 10:15:32 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+// Font Awesome
+import '@fortawesome/fontawesome-free/css/all.css'
+
+createApp(App).mount('#app')
+
+// Bootstrap
+import "bootstrap/dist/js/bootstrap.js";
+
