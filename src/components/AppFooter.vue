856eeea02ab28686154d62877842e9339b609d73
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-13 12:56:33 +0100

    creato componente bottom-footer e l'ho collegato al componente footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,18 @@
 <script>
-
+import bottomFooter from './bottomFooter.vue'
 
 
 export default {
 
     name: 'AppFooter',
 
-
+    components:{
+        bottomFooter,
+    },
 
     data() {
         return {
-            
+
         }
     },
 
@@ -19,7 +21,7 @@ export default {
 
     },
 
-    props:{
+    props: {
         footerLinks: Array,
     }
 
@@ -29,67 +31,68 @@ export default {
 </script>
 
 <template>
-<footer>
-    <div id="content-footer">
-        <div class="footer-logo">
-            <img src="../../public/img/logo (1).png" alt="">
-            <p>Yacht rentas for people who love sailing across the seas... </p>
-        </div>
+    <footer>
+        <div id="content-footer">
+            <div class="footer-logo">
+                <img src="../../public/img/logo (1).png" alt="">
+                <p>Yacht rentas for people who love sailing across the seas... </p>
+            </div>
 
-        <div id="footer-links" class="d-flex">
-            <ul v-for="category in footerLinks" :key="category.title">
-                <li v-for="link in category.links" :key="link.text">
-                    <a :href="link.url">{{ link.text }}</a>
-                </li>
-            </ul>
-        </div>
-        <div id="newsletter">
-            <h4>Newsletter</h4>
+            <div id="footer-links" class="d-flex">
+                <ul v-for="category in footerLinks" :key="category.title">
+                    <li v-for="link in category.links" :key="link.text">
+                        <a :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="newsletter">
+                <h4>Newsletter</h4>
 
-            <form>
-                <input type="text" placeholder="Enter your e-mail" class="border-0">
-                <button class="btn btn-outline-light ">Subcribe</button>
-            </form>
-        </div>
+                <form>
+                    <input type="text" placeholder="Enter your e-mail" class="border-0">
+                    <button class="btn btn-outline-light ">Subcribe</button>
+                </form>
+            </div>
 
-    </div>
-</footer>
+        </div>
+    </footer>
+    <bottomFooter></bottomFooter>
 </template>
 
 <style lang="scss">
 @use '../styles/mixins' as *;
 @use '../styles/variables' as *;
 
-footer{
+footer {
     background-color: #f5f5f5;
     padding: 140px 0;
 }
 
-#content-footer{
+#content-footer {
     @include styleContainer;
     display: flex;
     justify-content: space-between;
-    
 
 
-    
+
+
 
 }
 
-.footer-logo{
-    img{
-       width: 230px; 
-       margin-bottom: 30px;
+.footer-logo {
+    img {
+        width: 230px;
+        margin-bottom: 30px;
     }
 }
 
-#footer-links{
+#footer-links {
     gap: 50px;
 
-    ul{
+    ul {
         list-style: none;
 
-        a{
+        a {
             text-decoration: none;
             color: black;
             text-transform: uppercase;
@@ -100,21 +103,21 @@ footer{
 
 }
 
-#newsletter{
+#newsletter {
 
-    h4{
+    h4 {
         font-weight: 700;
     }
 
-    form{
+    form {
         display: flex;
     }
 
-    input{
+    input {
         padding: 10px 15px;
     }
 
-    button{
+    button {
         background-color: $PrimaryColor;
         padding: 10px 15px;
     }
---
+++ src/components/bottomFooter.vue
@@ -0,0 +1,128 @@
+<script>
+
+
+
+export default {
+
+    name: 'bottomFooter',
+
+
+
+    data() {
+        return {
+
+        }
+    },
+
+
+    methods: {
+
+    },
+
+
+
+}
+
+</script>
+
+<template>
+    <div id="bottom-footer">
+        <div class="content-bottom-footer">
+            <div class="policy">
+                <ul>
+                    <li><a href="#">zemez<span>@.all rights reserved</span></a></li>
+                    <li><a href="#">privacy policy</a></li>
+                    <li><a href="#">terms of use</a></li>
+                </ul>
+            </div>
+            <div id="socials" class="d-flex">
+                <div id="social-container" class="d-flex align-items-center justify-content-center">
+                    <i class="fa-brands fa-facebook-f">
+                        <a href="#"></a>
+                    </i>
+                </div>
+    
+                <div id="social-container" class="d-flex align-items-center justify-content-center">
+                    <i class="fa-brands fa-twitter">
+                        <a href="#"></a>
+                    </i>
+                </div>
+    
+                <div id="social-container" class="d-flex align-items-center justify-content-center">
+                    <i class="fa-brands fa-instagram">
+                        <a href="#"></a>
+                    </i>
+                </div>
+    
+    
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss">
+@use '../styles/mixins' as *;
+@use '../styles/variables' as *;
+
+
+#bottom-footer {
+    background-color: #222627;
+    padding: 30px 0;
+}
+
+.content-bottom-footer {
+    @include styleContainer;
+    display: flex;
+    justify-content: space-between;
+}
+
+.policy {
+    ul {
+        display: flex;
+
+        span {
+            color: #818075;
+        }
+
+        li {
+            padding: 0 20px;
+            border-right: 1px solid white;
+
+            &:first-of-type {
+                padding-left: 0;
+            }
+
+            &:last-of-type {
+                border-right: 0;
+            }
+        }
+
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        text-transform: uppercase;
+    }
+}
+
+#socials {
+
+    gap: 20px;
+    color: white;
+
+    #social-container {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: #6a6c71;
+
+        i {
+            font-size: 12px;
+        }
+    }
+
+
+}
+</style>
